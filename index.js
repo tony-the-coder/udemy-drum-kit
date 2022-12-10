18b529266b247d1ff09faef79b8daac5a679d710
Tony <tonythecoder@outlook.com>
2022-12-10 11:06:08 -0500

    Testing New Audio from CLI

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ var numberOfDrumbButtons = document.querySelectorAll(".drum").length;
 
 // console.log(numberOfDrumbButtons)
 
+// var audio = new Audio('./sounds/tom-1.mp3')
 
 for (let x = 0; x < numberOfDrumbButtons; x++) {
     document.querySelectorAll(".drum")[x].addEventListener("click", function () {
-        alert("I got clicked")
+        var audio = new Audio('./sounds/tom-1.mp3')
+        audio.play();
     })
 
+
 }
+
